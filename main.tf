dc5fb7c3b498f98ea7de108de9577737d89ef7d7
Dwight Spencer <dspencer@computekindustries.com>
2021-03-05 19:41:18 +0000

    (bugfix) move vars into main

--- main.tf
+++ main.tf
@@ -1,3 +1,33 @@
+variable "environment"     { default = "ustx-dev-lab" }
+variable "instance_type"   { default = "t2.medium" }
+variable "region"          { default = "us-east-1" }
+variable "monitoring"      { default = true }
+variable "scaling"	   {
+      type    = map
+      default = {
+            "min"    = 0
+            "max"    = 1
+      }
+}
+
+variable "aws_labels"      { 
+      type = map
+      default = {
+            "net.matrix.orgunit"      = "Matrix NOC"
+            "net.matrix.organization" = "Private Ops"
+            "net.matrix.commonname"   = "cloud"
+            "net.matrix.locality"     = "Dallas"
+            "net.matrix.state"        = "Texas"
+            "net.matrix.country"      = "USA"
+            "net.matrix.environment"  = "development"
+            "net.matrix.application"  = "windows desktop"
+            "net.matrix.role"         = "labs"
+            "net.matrix.owner"        = "FC13F74B@matrix.net"
+            "net.matrix.customer"     = "PVT-01"
+            "net.matrix.costcenter"   = "INT-01"
+      }
+}
+
 module "ecs-windows" {
   source	= "jjno91/ecs-windows/aws"
   version	= "0.1.0"
