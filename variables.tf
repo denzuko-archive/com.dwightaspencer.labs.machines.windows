e946027afc373e783ea77d5988fc96cc750fcfed
Dwight Spencer <dspencer@computekindustries.com>
2021-03-05 19:36:00 +0000

    (bugfix) remove old quoted maps.

--- variables.tf
+++ variables.tf
@@ -3,13 +3,13 @@ variable "instance_type"   { default = "t2.medium" }
 variable "region"          { default = "${env.AWS_DEFAULT_REGION}" }
 variable "monitoring"      { default = true }
 variable "scaling"	   {
-      type    = "map"
+      type    = map
       default = {
             "min"    = 0
             "max"    = 1
 }
 variable "aws_labels"      { 
-      type = "map"
+      type = map
       default = {
             "net.matrix.orgunit"      = "Matrix NOC"
             "net.matrix.organization" = "Private Ops"
@@ -17,9 +17,9 @@ variable "aws_labels"      {
             "net.matrix.locality"     = "Dallas"
             "net.matrix.state"        = "Texas"
             "net.matrix.country"      = "USA"
-            "net.matrix.environment"  = "${env.ENVIRONMENT}"
-            "net.matrix.application"  = "infrastructure"
-            "net.matrix.role"         = "application services"
+            "net.matrix.environment"  = vars.environment
+            "net.matrix.application"  = "development"
+            "net.matrix.role"         = "labs"
             "net.matrix.owner"        = "FC13F74B@matrix.net"
             "net.matrix.customer"     = "PVT-01"
             "net.matrix.costcenter"   = "INT-01"
