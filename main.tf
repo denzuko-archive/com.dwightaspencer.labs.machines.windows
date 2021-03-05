f5d9c5ac46c21667eac17e1abc4e33ea4193d79b
Dwight Spencer <dspencer@computekindustries.com>
2021-03-05 19:32:55 +0000

    (bugfix) change single to double quote marks

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ module "ecs-windows" {
   source	= "jjno91/ecs-windows/aws"
   version	= "0.1.0"
   env		= vars.environment
-  min_size	= vars.scaling['min']
-  max_size	= vars.scaling['max']
+  min_size	= vars.scaling["min"]
+  max_size	= vars.scaling["max"]
   instance_type	= vars.instance_type
   tags		= vars.aws_labels
 }
